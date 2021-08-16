4be2c4c2ba68bac9199d0a626ee9dfa3fee707f9
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-16 12:27:51 +0530

    update: binding html elements in js done

---
+++ app.js
@@ -0,0 +1,5 @@
+const birthDay = document.querySelector("#birthday")
+const luckyNumber = document.querySelector("#lucky-number")
+const button = document.querySelector("#btn-check")
+const message = document.querySelector("#message")
+
