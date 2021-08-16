440c7d15bf862140b8b21b30d075dc64a134d3f6
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-16 14:03:06 +0530

    wip function check

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const luckyNumber = document.querySelector("#lucky-number")
 const button = document.querySelector("#btn-check")
 const message = document.querySelector("#message")
 
-button.addEventListener("click", () => {
-    console.log (birthDay.valuealue, luckyNumber.value)
-})
+button.addEventListener("click", checkLuckyBirthDay)
+
+function checkLuckyBirthDay(){
+    const dob = birthDay.value;
+    const sum = calculateSum(dob)
+    console.log(sum)
+}
+
+function calculateSum (dob){
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let i=0; i<dob.length; i++){
+        sum = sum + Number(dob.charAt(i))
+    }
+
+    return sum
+
+}
